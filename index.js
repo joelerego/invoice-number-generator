30f88e1ded69fe411b53388affba50012f2ac957
Joel E. Rego <joelerego@gmail.com>
2020-09-01 20:23:27 +0530

    Fixed the race condition bug using atomic increment.

--- index.js
+++ index.js
@@ -6,30 +6,31 @@ const REDIS_PORT = process.env.PORT || 6379;
 
 const client = redis.createClient(REDIS_PORT);
 
+const { promisify } = require("util");
+const getAsync = promisify(client.get).bind(client);
+
 const app = express();
 
 // Set response
-function setResponse(username, count) {
+function setResponse(id, count) {
 
-    let base = username.substring(0, 4).toUpperCase();
-    let dateString = String(Date()).substring(4, 15).split(" ").join("").toUpperCase();
-    base = base + "-" + dateString + "-" + zeroPad(count, 4);
+    let date = new Date();
+    let invNum = id.substring(0, 4).toUpperCase() + "-" + date.getFullYear() + zeroPad(date.getMonth(), 2) + zeroPad(date.getDate(), 2) + "-" + zeroPad(count, 4);
 
-    let newCount = Number(count) + 1;
-    client.set(username, newCount);
+    client.incr(id);
 
-    return `${username}: ${base}`;
+    return `${id}: ${invNum}`;
 }
 
 async function getInvoiceNumber(req, res, next) {
     try {
-        const { username } = req.params;
+        const { id } = req.params;
         let count = 1;
 
         // Set data to Redis. The setex method as an expiration argument in seconds.
-        client.setex(username, count, 86400);
+        client.set(id, count);
 
-        res.send(setResponse(username, count));
+        res.send(setResponse(id, count));
 
     } catch (err) {
         console.error(err);
@@ -45,20 +46,18 @@ function zeroPad(number, width) {
     return string;
 }
 
-
-// Cache middleware
 function cache(req, res, next) {
-    const { username } = req.params;
-    client.get(username, (err, data) => {
+    const { id } = req.params;
+    client.get(id, (err, data) => {
         if (data !== null) {
-            res.send(setResponse(username, data));
+            res.send(setResponse(id, data));
         } else {
             next();
         }
     });
 }
 
-app.get('/user/:username', cache, getInvoiceNumber);
+app.get('/user/:id', cache, getInvoiceNumber);
 
 app.listen(5000, () => {
     console.log(`App listening on port ${PORT}`);
